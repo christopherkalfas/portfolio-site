94b2bbe227976aa906b68d6247e8e77a747db9e5
Chris Kalfas <christopherkalfas22@gmail.com>
2020-05-26 11:07:02 -0400

    add contact page with links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./contents/Home"
 import About from "./contents/About"
 import Education from "./contents/Education"
 import Skills from "./contents/Skills"
+import Contact from "./contents/Contact"
 
 function App() {
   return (
@@ -24,7 +25,10 @@ function App() {
         </Route>
         <Route exact path="/skills">
           <Skills />
-        </Route>                  
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>                     
       </div>
     </Router>
   );
---
+++ src/contents/contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Social from "../components/Social"
+
+const Contact = ()=> {
+    return(
+        <div className="condiv">
+            <h1 className="subtopic">Call me, beep me, if you wanna reach me. </h1>
+            <h2>When ya wanna page me it's okay.</h2>
+            <br></br>
+                <h3>Email: <a href="mailto:christopherkalfas22@gmail.com">christopherkalfas22@gmail.com</a></h3>
+                <h3>GitHub: <a href="https://github.com/christopherkalfas">christopherkalfas</a></h3>
+                <h3>Instagram: @kalfinator</h3>
+                <h3>Blog:<a href="https://christopherkalfas.github.io/">christopherkalfas.github.io</a> </h3>
+            <Social />
+
+        </div>
+    )
+}
+
+export default Contact
