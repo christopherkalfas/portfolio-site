92646f9029861351ccadd4d69cdbb3abe328f855
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-03 17:26:08 -0400

    added project links and laid out some card styling on them.

--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -9,21 +9,39 @@ const Projects = () => {
                 </h1>
             </div>
             <div className="project-container">
+            <div className="project-card">
+                    <a href="https://radiant-wave-27873.herokuapp.com/">
+                        <img src={require("../img/sweatclause.png")} alt="sweatclause"  className="project-image"/>
+                    </a>
+                    <div className="card-content">
+                        <h2 className="project-name">
+                            Sweat Clause
+                        </h2>
+                        <p className="project-desc">
+                        Rails web app that allows users a fun way to donate to charity through fitness competitions with friends
+                        </p>
+                        <p className="project-language">
+                            Rails - bcrypt - Heroku
+                        </p>
+                    </div>
+                </div>
                 <div className="project-card">
                     <a href="https://determined-shockley-872086.netlify.app/">
-                        <img src={require("../img/weatherkalf.png")} alt="weatherkalf" />
+                        <img src={require("../img/weatherkalf.png")} alt="weatherkalf" className="project-image" />
                     </a>
-                    <h3 className="project-name">
-                        WeatherKal
-                    </h3>
-                    <p className="project-desc">
-                        Weather forecast search web application.
-                    </p>
-                    <p className="project-language">
-                        React - openweatherAPI
-                    </p>
-    
+                    <div className="card-content">
+                        <h2 className="project-name">
+                            WeatherKalf
+                        </h2>
+                        <p className="project-desc">
+                            Simple Weather forecast web application.
+                        </p>
+                        <p className="project-language">
+                            React - openweatherAPI - Netlify
+                        </p>
+                    </div>
                 </div>
+                
             </div>
         </div>
     )
