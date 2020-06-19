ed389ac81542601a46ab9148740d79f01ee587e3
Chris Kalfas <christopherkalfas22@gmail.com>
2020-06-19 12:26:39 -0400

    updated project page with more external links and added trivia app.

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,11 +5,11 @@ class Social extends Component {
     render() {
         return (
             <div className="social">
-            <a href="https://github.com/christopherkalfas" target="_blank"><i className="fab fa-github"></i></a>
-            <a href="https://www.instagram.com/kalfinator/" target="_blank"><i className="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/ckalfas" target="_blank"><i className="fab fa-facebook-f"></i></a>
-            <a href="https://www.linkedin.com/in/christopherkalfas/" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-            <a href="https://christopherkalfas.github.io/" target="_blank"><i className="fas fa-blog"></i></a>
+            <a href="https://github.com/christopherkalfas" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/kalfinator/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/ckalfas" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
+            <a href="https://www.linkedin.com/in/christopherkalfas/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a>
+            <a href="https://christopherkalfas.github.io/" target="_blank" rel="noopener noreferrer"><i className="fas fa-blog"></i></a>
             </div>
             )
         }
--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -10,18 +10,49 @@ const Projects = () => {
             </div>
             <div className="project-container">
             <div className="project-card">
-                    <a href="https://radiant-wave-27873.herokuapp.com/">
+                    <a href="https://radiant-wave-27873.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                         <img src={require("../img/sweatclause.png")} alt="sweatclause"  className="project-image"/>
                     </a>
                     <div className="card-content">
                         <h2 className="project-name">
-                            Sweat Clause
+                        <a href="https://radiant-wave-27873.herokuapp.com/" target="_blank" rel="noopener noreferrer">Sweat Clause</a>
                         </h2>
                         <p className="project-desc">
                         Rails web app that allows users a fun way to donate to charity through fitness competitions with friends
                         </p>
                         <p className="project-language">
                             Rails - bcrypt - Heroku
+                        </p>
+                        <p className="project-github">
+                            <a href="https://github.com/christopherkalfas/sweatclause422" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            <a href="https://radiant-wave-27873.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live App</a>
+                        </p>
+                    </div>
+                </div>
+
+                <div className="project-card">
+                    <a href="https://trivkalf.firebaseapp.com/playall" target="_blank" rel="noopener noreferrer">
+                        <img src={require("../img/trivKalf.png")} alt="trivkalf" className="project-image" />
+                    </a>
+                    <div className="card-content">
+                    <h2 className="project-name">
+                        <a href="https://trivkalf.firebaseapp.com/playall" target="_blank" rel="noopener noreferrer">
+                            TrivKalf 
+                        </a>
+                    </h2>
+                
+
+                        <p className="project-desc">
+                            Trivia game with two modes of play. Random Play - allows users to answer questions from 24 different categories. Category Pick - allows user to answer questions from a topic of their choice. 
+                        </p>
+                        <p className="project-language">
+                            React - Open Trivia Database - Firebase
+                        </p>
+                        <p className="project-github">
+                            <a href="https://github.com/christopherkalfas/trivkalf" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            <a href="https://trivkalf.firebaseapp.com/playall" target="_blank" rel="noopener noreferrer">
+                                Live App 
+                            </a>
                         </p>
                     </div>
                 </div>
@@ -31,7 +62,7 @@ const Projects = () => {
                     </a>
                     <div className="card-content">
                         <h2 className="project-name">
-                            WeatherKalf
+                        <a href="https://determined-shockley-872086.netlify.app/" target="_blank" rel="noopener noreferrer">WeatherKalf</a>
                         </h2>
                         <p className="project-desc">
                             Simple Weather forecast web application.
@@ -39,21 +70,29 @@ const Projects = () => {
                         <p className="project-language">
                             React - openweatherAPI - Netlify
                         </p>
+                        <p className="project-github">
+                            <a href="https://github.com/christopherkalfas/weather-kalf-app" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            <a href="https://determined-shockley-872086.netlify.app/" target="_blank" rel="noopener noreferrer">Live App</a>
+                        </p>
                     </div>
                 </div>
                 <div className="project-card">
-                    <a href="https://christopherkalfas.github.io/gif-search-app/">
+                    <a href="https://christopherkalfas.github.io/gif-search-app/" target="_blank" rel="noopener noreferrer">
                         <img src={require("../img/kalfigifs.png")} alt="kalfigifs" className="project-image" />
                     </a>
                     <div className="card-content">
                         <h2 className="project-name">
-                            KalfiGifs
+                        <a href="https://christopherkalfas.github.io/gif-search-app/" target="_blank" rel="noopener noreferrer">KalfiGifs</a>
                         </h2>
                         <p className="project-desc">
                             Gif Search Engine Web Application.
                         </p>
                         <p className="project-language">
                             React - GIHPYAPI - GitHub Pages
+                        </p>
+                        <p className="project-github">
+                            <a href="https://github.com/christopherkalfas/gif-search-app" target="_blank" rel="noopener noreferrer">GitHub</a>
+                            <a href="https://christopherkalfas.github.io/gif-search-app/" target="_blank" rel="noopener noreferrer">Live App</a>
                         </p>
                     </div>
                 </div>
